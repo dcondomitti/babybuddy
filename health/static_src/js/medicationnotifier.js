4b6a2ec0189cca6a2929317c361d2c4836331a9d
Christopher C. Wells <chris@chrxs.net>
2019-06-19 20:42:31 -0700

    Add SweetAlerts2 dependency and blank MedicationNotifier object (WIP).

---
+++ health/static_src/js/medicationnotifier.js
@@ -0,0 +1,9 @@
+/* Baby Buddy Medication Notifier
+ *
+ * Handles creation and triggering of notifications about medication schedules.
+ */
+BabyBuddy.MedicationNotifier = function ($) {
+    var MedicationNotifier = {
+    };
+    return MedicationNotifier;
+}(django.jQuery);
